c99be84899d1816f83a7c2842bd8e221b4580eaa
amirsaber <amir.s.sharifi@gmail.com>
2014-08-01 02:25:26 +0000

    MongoHQ address for development environment

--- config/env/development.js
+++ config/env/development.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 	//db: 'mongodb://localhost/dry-depth-dev',
-	db: 'mongodb://admin:Amir271887@kahana.mongohq.com:10063/app27427657',
+	db: 'mongodb://heroku:svNj_vYR-jwgmQdTsGVQk9oh4DhDPl2HVCfDLV0ti5mMxdQFmlmMgsPlcd-7W_7vNGBGMzWk2bE0u4S-zm4L-g@kahana.mongohq.com:10063/app27427657',
 	app: {
 		title: 'Dry Depth - Development Environment'
 	},
